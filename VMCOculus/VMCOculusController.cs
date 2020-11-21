454142976bf48bf923058b63771adc74b1646e28
デンパ時計 <jsoldenpa@yahoo.co.jp>
2020-11-22 02:25:50 +0900

    プロジェクト ファイルを追加します。

---
+++ VMCOculus/VMCOculusController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.XR;
+using uOSC;
+
+namespace VMCOculus
+{
+    /// <summary>
+    /// Monobehaviours (scripts) are added to GameObjects.
+    /// For a full list of Messages a Monobehaviour can receive from the game, see https://docs.unity3d.com/ScriptReference/MonoBehaviour.html.
+    /// </summary>
+    public class VMCOculusController : MonoBehaviour
+    {
+        public static VMCOculusController Instance { get; private set; }
+
+        InputDevice _device;
+        uOscClient client;
+
+        // These methods are automatically called by Unity, you should remove any you aren't using.
+        #region Monobehaviour Messages
+        /// <summary>
+        /// Only ever called once, mainly used to initialize variables.
+        /// </summary>
+        private void Awake()
+        {
+            // For this particular MonoBehaviour, we only want one instance to exist at any time, so store a reference to it in a static property
+            //   and destroy any that are created while one already exists.
+            if (Instance != null) {
+                Plugin.Log?.Warn($"Instance of {GetType().Name} already exists, destroying.");
+                GameObject.DestroyImmediate(this);
+                return;
+            }
+            GameObject.DontDestroyOnLoad(this); // Don't destroy this object on scene changes
+            Instance = this;
+            this._device = InputDevices.GetDeviceAtXRNode(XRNode.CenterEye);
+            this.client = this.gameObject.AddComponent<uOscClient>();
+            Plugin.Log?.Debug($"{name}: Awake()");
+        }
+        /// <summary>
+        /// Called every frame if the script is enabled.
+        /// </summary>
+        private void Update()
+        {
+            try {
+                this._device.TryGetFeatureValue(CommonUsages.devicePosition, out var v);
+                this._device.TryGetFeatureValue(CommonUsages.deviceRotation, out var r);
+                client.Send("/VMC/Ext/Hmd/Pos", "HMD",
+                v.x, v.y, v.z,
+                r.x, r.y, r.z, r.w);
+            }
+            catch (Exception e) {
+                Plugin.Log.Error(e);
+            }
+        }
+
+
+        /// <summary>
+        /// Called when the script is being destroyed.
+        /// </summary>
+        private void OnDestroy()
+        {
+            Plugin.Log?.Debug($"{name}: OnDestroy()");
+            if (Instance == this)
+                Instance = null; // This MonoBehaviour is being destroyed, so set the static instance property to null.
+        }
+        #endregion
+    }
+}
---
+++ VMCOculus/uOsc/Scripts/uOscClient.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+
+namespace uOSC
+{
+
+    public class uOscClient : MonoBehaviour
+    {
+        private const int BufferSize = 8192;
+        private const int MaxQueueSize = 100;
+
+        [SerializeField]
+        string address = "127.0.0.1";
+
+        [SerializeField]
+        int port = 39540;
+
+#if NETFX_CORE
+    Udp udp_ = new Uwp.Udp();
+    Thread thread_ = new Uwp.Thread();
+#else
+        Udp udp_ = new DotNet.Udp();
+        Thread thread_ = new DotNet.Thread();
+#endif
+        ConcurrentQueue<object> messages_ = new ConcurrentQueue<object>();
+        void OnEnable()
+        {
+            udp_.StartClient(address, port);
+            thread_.Start(UpdateSend);
+        }
+
+        void OnDisable()
+        {
+            thread_.Stop();
+            udp_.Stop();
+        }
+
+        void UpdateSend()
+        {
+            while (messages_.TryDequeue(out var message)) {
+                using (var stream = new MemoryStream(BufferSize)) {
+                    if (message is Message) {
+                        ((Message)message).Write(stream);
+                    }
+                    else if (message is Bundle) {
+                        ((Bundle)message).Write(stream);
+                    }
+                    else {
+                        return;
+                    }
+                    udp_.Send(Util.GetBuffer(stream), (int)stream.Position);
+                }
+            }
+        }
+
+        void Add(object data)
+        {
+            messages_.Enqueue(data);
+            _ = Task.Run(() =>
+            {
+                while (messages_.Count > MaxQueueSize) {
+                    messages_.TryDequeue(out _);
+                }
+            });
+        }
+
+
+        public void Send(string address, params object[] values)
+        {
+            Send(new Message()
+            {
+                address = address,
+                values = values
+            });
+        }
+
+        public void Send(Message message)
+        {
+            Add(message);
+        }
+
+        public void Send(Bundle bundle)
+        {
+            Add(bundle);
+        }
+    }
+}
